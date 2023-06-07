6501115b3de96efc9e68838941249e6eddbca867
kylepretty <kylepretty@eastlink.ca>
2023-06-06 22:55:54 -0230

    tested working

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -4,8 +4,7 @@ const routes = [
     {path: '', name: 'home', component: () => import('@/views/Home.vue')},
     {path: '/team', name: 'team', component: () => import('@/views/Team.vue')},
     {path: '/car', name: 'car', component: () => import('@/views/Car.vue')},
-    {path: '/sponsors', name: 'sponsors', component: () => import('@/views/Sponsors.vue')},
-    
+    {path: '/sponsors', name: 'sponsors', component: () => import('@/views/Sponsors.vue')},  
 ]
 
 const router = createRouter({
