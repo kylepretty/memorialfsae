f935b01c2cd1a9f039dc03f808a5601fb4b57c31
kylepretty <kylepretty@eastlink.ca>
2023-06-06 22:36:45 -0230

    tested working

--- dist/assets/Car-f794a653.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,a as o}from"./index-7dd71d10.js";const c={};function t(a,n){return e(),o("h1",null,"Our Car")}const f=r(c,[["render",t]]);export{f as default};
