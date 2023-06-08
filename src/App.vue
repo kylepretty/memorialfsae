6a2838ba065adffcd454c750c487a05e8749f4cb
kylepretty <kylepretty@eastlink.ca>
2023-06-08 14:14:29 -0230

    fixed navbar size on small screens

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
       <body class="bg-zinc-900 text-white sticky top-0">
         <nav class="py-6 px-6 flex justify-between items-center bg-zinc-900">
-            <a href="/" class="text-xl font-semibold">FormulaMUN</a>
+            <router-link :to="{name: 'home'}" class="text-lg sm:text-xl font-semibold">FormulaMUN</router-link>
             <div class="space-x-6">
-                <router-link :to="{name: 'home'}" class="text-lg hover:text-gray-500">Home</router-link>
-                <router-link :to="{name: 'car'}" class="text-lg hover:text-gray-500">Our Car</router-link>
-                <router-link :to="{name: 'team'}" class="text-lg hover:text-gray-500">Our Team</router-link>
-                <router-link :to="{name: 'sponsors'}" class="text-lg hover:text-gray-500">Sponsorships</router-link>
+                <router-link :to="{name: 'car'}" class="text-md sm:text-lg hover:text-gray-500">Our Car</router-link>
+                <router-link :to="{name: 'team'}" class="text-md sm:text-lg hover:text-gray-500">Our Team</router-link>
+                <router-link :to="{name: 'sponsors'}" class="text-md sm:text-lg hover:text-gray-500">Sponsorships</router-link>
             </div>
         </nav>
     </body>
